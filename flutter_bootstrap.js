188002530b7ca8853f491ab6460115abccea4de1
Tardan <tardyadam2@gmail.com>
2024-06-24 23:08:41 +0200

    Deploying web app to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "626652889"
+    serviceWorkerVersion: "2348813691"
   }
 });
